dbf39997ec54eecb7b28de913fed9a269d7c1185
Balazs Varga <b4zs@b4zs.com>
2015-09-01 22:05:48 +0200

    deploy fixes

--- src/Application/CrmBundle/Controller/DefaultController.php
+++ src/Application/CrmBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace Application\CrmBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ApplicationCrmBundle:Default:index.html.twig');
+        return new RedirectResponse($this->container->get('router')->generate('sonata_admin_dashboard'));
     }
 }
