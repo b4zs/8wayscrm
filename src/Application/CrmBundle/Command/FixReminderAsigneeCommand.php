b3e25505d902771fc73f6c6bdacd8726c19076fe
Tuza János <tuza.janos@gb-art.hu>
2016-09-29 12:13:56 +0200

    migrate commands fix

--- src/Application/CrmBundle/Command/FixReminderAsigneeCommand.php
+++ src/Application/CrmBundle/Command/FixReminderAsigneeCommand.php
@@ -13,7 +13,19 @@ class FixReminderAsigneeCommand extends AbstractTicketingMigrateCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $reminders = $this->getDoctrine()->getRepository('OctetTicketingBundle:Reminder')->findAll();
 
+        foreach ($reminders as $reminder) {
+
+            $assigneeOids = $this->getPdo()->query(sprintf('SELECT * FROM %s WHERE subject = %d', static::REMINDER_TEMP_TABLE, $reminder->getId()));
+
+            foreach($assigneeOids as $row) {
+                $objectIdentity = $this->getDoctrine()->getRepository('ApplicationObjectIdentityBundle:ObjectIdentity')->find($row['oid']);
+                $reminder->setAssignee($objectIdentity->getReference());
+            }
+
+            $this->getDoctrine()->getManager()->flush($reminder);
+        }
     }
 
 
