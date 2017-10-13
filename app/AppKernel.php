a11ad501d19432a707884da9e3f076f4266fe6c1
David Kovacs <kovacs.david@gb-art.hu>
2017-10-13 09:56:09 +0200

    add migrations and fixture generation bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,4 +1,4 @@
-    <?php
+<?php
 
 use Application\AdminBundle\ApplicationAdminBundle;
 use Application\MediaBundle\ApplicationMediaBundle;
@@ -24,6 +24,8 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
+//            new CoreSecurityBundle(),
+//            new CoreToolsBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -33,41 +35,32 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-	        new KnpMenuBundle(),
+            new KnpMenuBundle(),
             new JMSSerializerBundle(),
             new FOSRestBundle(),
-	        new SonataCoreBundle(),
-			new SonataAdminBundle(),
-	        new SonataBlockBundle(),
-	        new SonataDoctrineORMAdminBundle(),
+            new SonataCoreBundle(),
+            new SonataAdminBundle(),
+            new SonataBlockBundle(),
+            new SonataDoctrineORMAdminBundle(),
             new Application\ProjectAccountingBundle\ApplicationProjectAccountingBundle(),
             new Application\CrmBundle\ApplicationCrmBundle(),
-//            new CoreSecurityBundle(),
-//            new CoreToolsBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-
-            ///media
-            // ...
             new Sonata\ClassificationBundle\SonataClassificationBundle(),
             new Sonata\MediaBundle\SonataMediaBundle(),
             new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
             new Sonata\IntlBundle\SonataIntlBundle(),
             new ApplicationMediaBundle(),
-
             new FOSUserBundle(),
             new SonataUserBundle(),
             new ApplicationUserBundle(),
-
             new ApplicationAdminBundle(),
             new Core\LoggableEntityBundle\CoreLoggableEntityBundle(),
-
             new Core\ToolsBundle\CoreToolsBundle(),
             new Octet\MessageBusBundle\OctetMessageBusBundle(),
             new SimpleBus\AsynchronousBundle\SimpleBusAsynchronousBundle(),
             new SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle(),
             new SimpleBus\SymfonyBridge\SimpleBusEventBusBundle(),
             new OctetTicketingBundle(),
-
             new Core\ObjectIdentityBundle\CoreObjectIdentityBundle(),
             new Application\ObjectIdentityBundle\ApplicationObjectIdentityBundle(),
             new Application\RedmineIntegrationBundle\ApplicationRedmineIntegrationBundle(),
@@ -75,7 +68,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Application\QuotationGeneratorBundle\ApplicationQuotationGeneratorBundle(),
-
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -83,6 +75,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Webonaute\DoctrineFixturesGeneratorBundle\DoctrineFixturesGeneratorBundle();
         }
 
         return $bundles;
